74b29ba3e77990b48b4bf447fffa0c30b76e382d
mena afefe fawze <73434035+MNaguib2@users.noreply.github.com>
2022-02-24 00:47:29 +0200

    finish tab

--- FrontEnd/src/app/courses/classes/classes.component.ts
+++ FrontEnd/src/app/courses/classes/classes.component.ts
@@ -13,4 +13,15 @@ Materials=['Material1', 'Material2', 'Material3', 'Material4'];
 
   ngOnInit(): void {
   }
+  old: any;
+  new : any;
+  Action(event: any){
+    if(!this.old){
+      this.old = $(`#${event.explicitOriginalTarget.id}`);
+    }else{
+      this.old.attr("aria-selected", false);
+      this.old.removeClass("active");
+      this.old = $(`#${event.explicitOriginalTarget.id}`);
+    }
+  }
 }
